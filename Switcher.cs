af176893f12904863a48ccca295489cf23250b3e
Shichiha <77842398+Shichiha@users.noreply.github.com>
2022-06-19 01:51:09 +0900

    Base Form

---
+++ Switcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace ProfileSwitcher
+{
+    public partial class Switcher : MaterialForm
+    {
+        public Switcher()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+        }
+
+        private void Switcher_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TabSelector_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ProfileSwitcher
+{
+    public class Utility
+    {
+        public string Name { get; set; }
+        public string MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810 { get; set; }
+        public static Utility ReadFromDisk(string name)
+        {
+            string input = File.ReadAllText(Path.Combine(Application.StartupPath, "UserData", name));
+            return new JavaScriptSerializer().Deserialize<Utility>(input);
+        }
+        public void WriteToDisk()
+        {
+            File.WriteAllText(Path.Combine(Application.StartupPath, "UserData", this.Name), new JavaScriptSerializer().Serialize(this));
+        }
+        public static void DeleteFromDisk(string name)
+        {
+            File.Delete(Path.Combine(Application.StartupPath, "UserData", name));
+        }
+        public static Utility ReadFromRegedit()
+        {
+            return new Utility
+            {
+                MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810 = Utility.GetStringFromRegedit("MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810")
+            };
+        }
+        public void WriteToRegedit()
+        {
+            if (string.IsNullOrWhiteSpace(this.MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810))
+            {
+                MessageBox.Show("Account not found", "Account Manager", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            Utility.SetStringToRegedit("MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810", this.MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810);
+            MessageBox.Show("Account [" + this.Name + "] switched successfully", "Account Manager", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+        private static string GetStringFromRegedit(string key)
+        {
+            object value = Registry.GetValue("HKEY_CURRENT_USER\\Software\\miHoYo\\Genshin Impact", key, "");
+            return Encoding.UTF8.GetString((byte[])value);
+        }
+        private static void SetStringToRegedit(string key, string value)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\miHoYo\\Genshin Impact", key, Encoding.UTF8.GetBytes(value));
+        }
+    }
+}
